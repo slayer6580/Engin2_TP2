e9c55b47b7cd28ffe01bd26956bf4c2dbad24a8d
mqcfrank <88257038+mqcfrank@users.noreply.github.com>
2024-02-01 23:11:50 -0500

    Canon+canonball, changed prefab bumper, added bumper start of level for testing

--- Engin2_TP2/Assets/Scripts/Interactables/Bumper.cs
+++ Engin2_TP2/Assets/Scripts/Interactables/Bumper.cs
@@ -9,7 +9,10 @@ public class Bumper : MonoBehaviour, IInteractable
      * On pourrait plutôt créer un script "Bumpable" qui déterminerait quel object est repoussé par le bumper
      */
     [SerializeField] private float pushForce = 10f; // Adjustable force of the push
+    [SerializeField] private float bumperForceReduction = 2.0f; // le bumper bouge moins que le player sur impact
     [SerializeField] private bool bumperObjectMovesOnImpact = true;
+    [SerializeField] private bool canBeMovedByGameMaster = false;
+
     private Rigidbody bumperRigidbody;
     private BoxCollider bumperCollider; // jai essayé un mesh collider, trop lent
     
@@ -18,7 +21,7 @@ public class Bumper : MonoBehaviour, IInteractable
     public void Awake()
     {
          bumperRigidbody = this.GetComponent<Rigidbody>(); 
-        bumperCollider = this.GetComponent<BoxCollider>();
+         bumperCollider = this.GetComponent<BoxCollider>();
     }
 
     public void OnPlayerCollision(Player player)
@@ -51,7 +54,7 @@ public class Bumper : MonoBehaviour, IInteractable
         Player player = collision.collider.GetComponent<Player>(); // le player doit avoir un script nommé Player sur lui
         if (player != null)
         {
-            OnPlayerCollision(player); // ou encore, appeler IBumpable sur le joueur?
+            OnPlayerCollision(player); // ou encore, appeler IBumpable sur le joueur? A-t-on vraiment besoin d'une sous-fonction ici? Le bumper ne bump que les players
         }
     }
 
@@ -62,8 +65,7 @@ public class Bumper : MonoBehaviour, IInteractable
         // ...
 
         // Pour le moment, on mets le script sur un objet; c'est un autre script sur l'objet qui permet le déplacement.
-
-
+        //IInteractable
     }
 
     public void UpdateInteractableObject(Player player)
@@ -71,11 +73,8 @@ public class Bumper : MonoBehaviour, IInteractable
        
         if (bumperRigidbody != null && bumperObjectMovesOnImpact)
         {
-
             Vector3 bumpForce = CalculateBumpForce(player);
-
-            // Apply force to the player's rigidbody
-            bumperRigidbody.AddForce(bumpForce, ForceMode.Impulse);
+            bumperRigidbody.AddForce(-bumpForce/bumperForceReduction, ForceMode.Impulse);
         }
 
     }
@@ -100,7 +99,7 @@ public class Bumper : MonoBehaviour, IInteractable
 
 	public void OnPlayerClicked(GameMasterController player)
 	{
-		throw new System.NotImplementedException();
+		throw new System.NotImplementedException(); // drag and drop?
 	}
 
 	public void OnPlayerClickUp(GameMasterController player)
