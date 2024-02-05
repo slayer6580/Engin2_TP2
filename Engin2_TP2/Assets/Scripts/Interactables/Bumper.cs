89fc60f49a80ae0e057529b5859b60e51e9af385
mqcfrank <88257038+mqcfrank@users.noreply.github.com>
2024-02-05 12:45:40 -0500

    Removed unused section of bumper method/class

--- Engin2_TP2/Assets/Scripts/Interactables/Bumper.cs
+++ Engin2_TP2/Assets/Scripts/Interactables/Bumper.cs
@@ -5,7 +5,7 @@ using UnityEngine;
      * Note: Le script va repousser tout ce qui a un PlayerStateMachine et un rigidbody
      * Attention: l'objet sur lequel ce script est mis doit avoir un rigidbody!!
 */
-public class Bumper : MonoBehaviour, IInteractable
+public class Bumper : MonoBehaviour
 {
     
     [SerializeField] private float pushForce = 1000f; // Adjustable force of the push
@@ -15,6 +15,7 @@ public class Bumper : MonoBehaviour, IInteractable
 
     private Rigidbody bumperRigidbody;
     public ParticleSystem touchEffect;
+
     public void Awake()
     {
          bumperRigidbody = this.GetComponent<Rigidbody>();    
@@ -37,21 +38,6 @@ public class Bumper : MonoBehaviour, IInteractable
         }
     }
 
-
-    // Code de l'interface. Pour les bumpers controllés par les Game Masters:
-    public void OnPlayerClicked(Player player)
-    { 
-        // ne fait rien, mais éventuellement, les maîtres du jeu pourraient déplacer le bumper   
-        // if (player.type == maitre)
-        // ...
-
-        // Pour le moment, on mets le script sur un objet; c'est un autre script sur l'objet qui permet le déplacement.
-        //IInteractable
-    }
-    public void OnPlayerCollision(Player player)
-    {
-        // On passe par la state machine finalement
-    }
     void PlayParticleEffect()
     {
         if (touchEffect != null)
@@ -60,18 +46,8 @@ public class Bumper : MonoBehaviour, IInteractable
         }
     }
 
-	public void OnPlayerClicked(GameMasterController player)
-	{
-		throw new System.NotImplementedException(); // drag and drop?
-	}
 
-	public void OnPlayerClickUp(GameMasterController player)
-	{
-		throw new System.NotImplementedException();
-	}
 
-	public void UpdateInteractableObject(GameMasterController player)
-	{
-		throw new System.NotImplementedException();
-	}
+
+
 }
